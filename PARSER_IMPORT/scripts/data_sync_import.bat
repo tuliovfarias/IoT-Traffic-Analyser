d64966c00022925d966b67bf2f7a73b1053f9b34
Túlio Vieira <tuliovfarias@users.noreply.github.com>
2021-02-26 00:41:34 -0300

    update

--- PARSER_IMPORT/scripts/data_sync_import.bat
+++ PARSER_IMPORT/scripts/data_sync_import.bat
@@ -1,15 +1,18 @@
 @echo off
 
-echo [%date:~6,10%-%date:~3,2%-%date:~0,2% %time:~0,8%] Runing scripts...
+cd ..
+SET LOG_FILE=%CD%\logs\data_sync_import.log
 
-call C:\cygwin64\bin\bash.exe -l "%CD%\sync.sh"
+ECHO -------------------------------------------------- >> %LOG_FILE%
+echo [%date:~6,10%-%date:~3,2%-%date:~0,2% %time:~0,8%] Runing scripts... >> %LOG_FILE%
+
+call C:\cygwin64\bin\bash.exe -l "%CD%\scripts\sync.sh >> %LOG_FILE%" 
 :: Activate python env in cmd
-call C:\Users\tulio\Anaconda3\Scripts\activate.bat
+::call %HomePath%\Anaconda3\Scripts\activate.bat
 :: Execute MySQL import python script
-python "%CD%\MySQL_data_import_V3.py"
+python "%CD%\scripts\MySQL_data_import_V3.py" >> %LOG_FILE%
 
-echo [%date:~6,10%-%date:~3,2%-%date:~0,2% %time:~0,8%] Scripts finished!
-echo.
+echo [%date:~6,10%-%date:~3,2%-%date:~0,2% %time:~0,8%] Scripts finished! >> %LOG_FILE%
 
-::pause
+::PAUSE
 exit
