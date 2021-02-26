c3875dc42f6c2a6b80180ac10d5152bd9006090c
Túlio Vieira <tuliovfarias@users.noreply.github.com>
2021-02-26 00:48:01 -0300

    update

--- PARSER_IMPORT/scripts/data_sync_import.bat
+++ PARSER_IMPORT/scripts/data_sync_import.bat
@@ -6,7 +6,7 @@ SET LOG_FILE=%CD%\logs\data_sync_import.log
 ECHO -------------------------------------------------- >> %LOG_FILE%
 echo [%date:~6,10%-%date:~3,2%-%date:~0,2% %time:~0,8%] Runing scripts... >> %LOG_FILE%
 
-call C:\cygwin64\bin\bash.exe -l "%CD%\scripts\sync.sh >> %LOG_FILE%" 
+call C:\cygwin64\bin\bash.exe -l "%CD%\scripts\sync.sh >> %LOG_FILE%"
 :: Activate python env in cmd
 ::call %HomePath%\Anaconda3\Scripts\activate.bat
 :: Execute MySQL import python script
