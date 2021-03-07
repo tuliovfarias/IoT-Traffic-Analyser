7793a03877c324de020270d04126aa128b395f2c
Túlio Vieira <tuliovfarias@users.noreply.github.com>
2021-03-06 22:19:13 -0300

    Update MySQL_data_import_V3.py

--- PARSER_IMPORT/scripts/MySQL_data_import_V3.py
+++ PARSER_IMPORT/scripts/MySQL_data_import_V3.py
@@ -10,8 +10,12 @@ import glob
 
 warnings.filterwarnings("ignore") ##Desabilita mensagens de Warning
 sys.exit
+TABLE_ALL_SCANS='all_scans'
+TABLE_MAC_COUNT='mac_count'
+TABLE_TEMP='temp'
 SCRIPT_PATH= __file__
-BASE_DIR=SCRIPT_PATH.split("\scripts")[0]
+#BASE_DIR=SCRIPT_PATH.split("\scripts")[0]
+BASE_DIR=os.path.dirname(os.path.dirname(SCRIPT_PATH))
 SOURCE_DIR= os.path.join(BASE_DIR,'data')
 TARGET_DIR= os.path.join(BASE_DIR,'imported')
 
@@ -26,7 +30,7 @@ c = create_engine('mysql+pymysql://root:computer@localhost/db_test')
 c.execute('USE db_test;')
 print("Connected!")
 col_names=['datetime','device_name','MAC', 'type', 'RSSI']
-c.execute("""CREATE TABLE IF NOT EXISTS mac_count(
+query_create_mac_count=("CREATE TABLE IF NOT EXISTS "+TABLE_MAC_COUNT+"""(
             datetime datetime DEFAULT '0000-00-00 00:00:00',
             device_name varchar(50),
             MAC varchar(50),
@@ -35,6 +39,7 @@ c.execute("""CREATE TABLE IF NOT EXISTS mac_count(
             count int,
             PRIMARY KEY (device_name,datetime,MAC)
             );""")
+c.execute(query_create_mac_count)
 print("Importing data to database...")
 #list_files = os.listdir(SOURCE_DIR)
 list_files = glob.glob(os.path.join(SOURCE_DIR,'*.csv'))
@@ -51,20 +56,19 @@ else:
         df.MAC.fillna("0", inplace = True) # substituir NULL por 0 no MAC (evitar erro se MAC cannot be NULL) 
         df.device_name.fillna("0", inplace = True) # substituir NULL por 0 no MAC (evitar erro se deviceID cannot be NULL) 
         #df.reset_index(drop=True, inplace=True) #remover maldito index
-        df.to_sql(name='table_temp', if_exists='append', con=c, index=False) # tabela temporária apenas com dados do arquivo atual
-        df.to_sql(name='table_all_scans', if_exists='append', con=c, index=False) # tabela com todos os dados
-        #c.execute('ALTER TABLE db_test.table_temp MODIFY COLUMN `datetime` datetime;')
-        query_mac_count=("""INSERT INTO mac_count
+        df.to_sql(name=TABLE_TEMP, if_exists='append', con=c, index=False) # tabela temporária apenas com dados do arquivo atual
+        df.to_sql(name=TABLE_ALL_SCANS, if_exists='append', con=c, index=False) # tabela com todos os dados
+        #c.execute('ALTER TABLE db_test.'+TABLE_TEMP+' MODIFY COLUMN `datetime` datetime;')
+        query_mac_count=("INSERT INTO "+TABLE_MAC_COUNT+"""
                     SELECT *,COUNT(MAC)
-                    FROM db_test.table_temp
+                    FROM db_test."""+TABLE_TEMP+"""
                     WHERE datetime BETWEEN '"""+start_datetime+"' and '"+end_datetime+"""'
                     GROUP BY device_name,MAC;""")
         c.execute(query_mac_count) # tabela com os MACs distintos e sua contagem (para cada arquivo)
-        c.execute("TRUNCATE TABLE db_test.table_temp;")
+        c.execute("TRUNCATE TABLE db_test."+TABLE_TEMP+";")
     #c.execute("""CREATE TABLE IF NOT EXISTS mac_count AS
     #          SELECT *, COUNT(MAC) AS qtd FROM db_test.table_test 
     #         GROUP BY `MAC`;""")
         shutil.move(file_path,os.path.join(TARGET_DIR,os.path.basename(file_path)))
         print(file_path)
     print("Imported!")
-# %%
